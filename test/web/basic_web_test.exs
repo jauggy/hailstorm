3083e54353fd1aa007b13d2d3819cc2e163c10ce
Teifion <teifion@teifion.co.uk>
2023-08-27 10:38:44 +0100

    Updated based on site url changes

--- test/web/basic_web_test.exs
+++ test/web/basic_web_test.exs
@@ -13,11 +13,16 @@ defmodule Hailstorm.Tests.BasicWebTest do
     # Assert we'll raise a 404 with our helper functions
     assert_raise RuntimeError, fn -> get_html("/this_path_does_not_exist") end
 
-    # Now check the site as a whole works as expected
+    # Now check the site as a whole works as expected, we should be forwarded to the login screen
     resp = get("/")
+    assert resp.status_code == 302
+    location = resp.headers |> Map.new |> Map.get("location")
+    assert location == "/login"
+
+    resp = get("/login")
     assert resp.status_code == 200
 
-    resp = get_html("/")
-    assert resp =~ "welcome-heading display-4"
+    resp = get_html("/login")
+    assert resp =~ "<div class=\"row\" style=\"padding-top: 15vh;\" id=\"login-div\">"
   end
 end
