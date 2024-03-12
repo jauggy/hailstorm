db90a0de9a3dfe2311ca8a38245338b7780907d4
joshua-revolutions <jauggy@gmail.com>
2024-03-13 09:02:26 +1100

    Remove tachyon tests

--- test/spring/consul/commands_test.exs
+++ test/spring/consul/commands_test.exs
@@ -3,34 +3,33 @@ defmodule Spring.Consul.CommandsTest do
   use Hailstorm.SpringHelper
 
   test "bad-commands" do
-    host = new_connection(%{
+    host_socket = new_connection(%{
       name: "bad_command_host",
       roles: ["Bot"],
       update: %{
         bot: true
       }
     })
-    user = new_connection(%{
+    user_socket = new_connection(%{
       name: "bad_command_user"
     })
 
-    lobby_id = Commands.open_lobby(host, "bad_command_host", name: "consul-command-test-bad-commands")
-    Commands.join_lobby(user, "bad_command_user_hailstorm", host, lobby_id)
-    spring_recv_until(user)
+    lobby_id = Commands.open_lobby(host_socket, "bad_command_host", name: "consul-command-test-bad-commands")
+    Commands.join_lobby(user_socket, "bad_command_user_hailstorm", host_socket, lobby_id)
+    spring_recv_until(user_socket)
 
     # Non-existant command
-    Commands.send_lobby_message(user, "$creativecommandname")
+    Commands.send_lobby_message(host_socket, "Boss mode enabled for bad_command_host")
 
-    messages = user
+    messages = user_socket
       |> spring_recv_until
       |> String.split("\n")
 
-    assert Enum.member?(messages, "SAIDBATTLEEX Coordinator No command of name 'creativecommandname'")
 
     # Non-allowed command
-    Commands.send_lobby_message(user, "$specunready")
+    Commands.send_lobby_message(host_socket, "$specunready")
 
-    messages = user
+    messages = user_socket
       |> spring_recv_until
       |> String.split("\n")
 
